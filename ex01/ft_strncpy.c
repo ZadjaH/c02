f7ee67e9840bedf4fef95e7ac935b63fdfffa58f
calcifer <cazinlea0@gmail.com>
2024-03-24 16:55:02 +0100

    init ex01

---
+++ ex01/ft_strncpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calcifer <calcifer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/24 16:29:15 by calcifer          #+#    #+#             */
+/*   Updated: 2024/03/24 16:54:23 by calcifer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	dest[10];
+	char	src[] = "caca";
+
+	printf("dest:%s\n", ft_strncpy(dest, src, 10));
+}
+*/
