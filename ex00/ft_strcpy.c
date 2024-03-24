c90772c98c7fa68533a8b136ba57e23a304a4ac3
calcifer <cazinlea0@gmail.com>
2024-03-24 16:27:51 +0100

    init ex00

---
+++ ex00/ft_strcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calcifer <calcifer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/24 14:23:49 by calcifer          #+#    #+#             */
+/*   Updated: 2024/03/24 16:26:49 by calcifer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	dest[5];
+
+	*ft_strcpy(dest, "caca");
+	printf("dest:%s\n", dest);
+}
+*/
